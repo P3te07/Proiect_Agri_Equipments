1a0a0b3a131343b159d2388a5655560312078aee
P3te07 <petruodobescu@gmail.com>
2025-10-07 08:59:59 +0300

    Structura de baza html

--- src/app/endpoints/contact/contact.component.ts
+++ src/app/endpoints/contact/contact.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-contact',
-  standalone: true, 
-  imports: [],               
+  standalone: true,
+  imports: [CommonModule, FormsModule, RouterLink],
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
-export class ContactComponent { }
+export class ContactComponent {
+  name = '';
+  email = '';
+  message = '';
+
+  submit() {
+    alert(`Mesaj trimis!\nNume: ${this.name}\nEmail: ${this.email}\nMesaj: ${this.message}`);
+
+    this.name = '';
+    this.email = '';
+    this.message = '';
+  }
+}
--- src/app/endpoints/equipment/add-equipment/add-equipment.component.ts
+++ src/app/endpoints/equipment/add-equipment/add-equipment.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-add-equipments',
   standalone: true, 
-  imports: [],               
+  imports: [FormsModule],               
   templateUrl: './add-equipment.component.html',
   styleUrls: ['./add-equipment.component.css']
 })
-export class AddEquipmentComponent { }
+export class AddEquipmentComponent { 
+  newEquipment = {
+    name: '',
+    description: '',
+    pricePerDay: 0,
+    imageUrl: '',
+    available: false
+  };
+
+  onSubmit() {
+    console.log('Echipament adăugat:', this.newEquipment);
+  }
+
+}
